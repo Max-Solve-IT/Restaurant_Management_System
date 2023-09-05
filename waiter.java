58fabaecc8ac42a2dc8a75e1808f4b8f90aa6c32
Swajan Baruah <70163983+swajan-75@users.noreply.github.com>
2023-09-05 13:06:15 +0600

    Update waiter.java

--- waiter.java
+++ waiter.java
@@ -1,23 +1,65 @@
 import java.util.Scanner;
-
-class waiter extends Employee {
+class waiter extends Employee{
+    waiter(){
+        this.set_position("Waiter");
+    }
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
+        System.out.println("------------------ Add Waiter --------------");
         waiter[] Waiters = new waiter[3];
-        for(int i = 0; i < 3; i++){
+            for(int i = 0; i < 3; i++){
             Waiters[i] = new waiter();
-            System.out.print("Waiter " + (i+1) + " Position: ");
-            String pos = cin.nextLine();
-            Waiters[i].set_position(pos);
+            System.out.print("------------------- Waiter " + (i+1) + " -------------------");
+            String s = new String();
+            System.out.print("\nName : ");
+            s = cin.nextLine();
+            Waiters[i].set_name(s);
+
+            System.out.print("\nAddress : ");
+            s = cin.nextLine();
+            Waiters[i].set_address(s);
+
+            System.out.print("\nPhone : ");
+            s = cin.nextLine();
+            Waiters[i].set_mobile(s);
+
+            System.out.print("\nSex : ");
+            s = cin.nextLine();
+            Waiters[i].set_sex(s);
+
+            System.out.print("\nBlood Group : ");
+            s = cin.nextLine();
+            Waiters[i].set_blood_group(s);
+
+            System.out.print("\nNationality : ");
+            s = cin.nextLine();
+            Waiters[i].set_nationality(s);
+
+            System.out.print("\nReligion : ");
+            s = cin.nextLine();
+            Waiters[i].set_religion(s);
+
+            System.out.print("\nAge : ");
+            int num;
+            num = cin.nextInt();
+            Waiters[i].set_age(num);
+            cin.nextLine();
         }
 
-        System.out.println("--------------------------------");
+        System.out.println("--------------------------------\n");
 
         for(int i = 0; i < 3; i++){
-            System.out.print("Waiter " + (i+1) + " Position: ");
-            System.out.println(Waiters[i].position);
-            System.out.print("Salary: ");
-            System.out.println(Waiters[i].get_Salary());
+            System.out.print("Waiter " + (i+1) + " Name : "+Waiters[i].get_name()+"\n");
+            System.out.print("Waiter " + (i+1) + " Age : "+Waiters[i].get_age()+"\n");
+            System.out.print("Waiter " + (i+1) + " Address : "+Waiters[i].get_address()+"\n");
+            System.out.print("Waiter " + (i+1) + " Sex : "+Waiters[i].get_sex()+"\n");
+            System.out.print("Waiter " + (i+1) + " Nationality : "+Waiters[i].get_nationality()+"\n");
+            System.out.print("Waiter " + (i+1) + " Religion : "+Waiters[i].get_religion()+"\n");
+            System.out.print("Waiter " + (i+1) + " Blood Group : "+Waiters[i].get_blood_group()+"\n");
+            System.out.print("Waiter " + (i+1) + " Mobile : "+Waiters[i].get_mobile()+"\n");
+            System.out.print("Waiter " + (i+1) + " Salary : "+Waiters[i].get_Salary()+"\n");
+            System.out.print("Waiter " + (i+1) + " Position : "+Waiters[i].get_position()+"\n \n");
+
         }
     }
 }
